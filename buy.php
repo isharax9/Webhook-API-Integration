f61522027df1084f73be587b1e8735bb7ed0608e
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2023-09-21 21:23:49 +0530

    Pause the project.
    📌due to exam

--- buy.php
+++ buy.php
@@ -14,10 +14,15 @@
 
 <h1>CC Payment here</h1>
 
+https://admin.ccpayment.com/ccpayment/v1/concise/url/get
 
-
-
-
+{
+    "order_valid_period":823456,
+    "product_price":"1",
+    "merchant_order_id":"2012033040550",
+    "product_name":"test",
+    "return_url":"https://checkout.ccpayment.com/xxxxx"
+}
 
 
 
